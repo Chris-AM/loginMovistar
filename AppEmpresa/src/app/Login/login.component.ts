1bfec96227a72f13c98c6cd0a4d336a80ffee2f9
Juan Vasquez Lagos <jvasquel@everis.com>
2020-11-02 15:00:47 -0300

    login

---
+++ AppEmpresa/src/app/Login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { RouterExtensions } from "@nativescript/angular";
+import { Page, TextField } from "@nativescript/core";
+import { LoginService } from "./login.service";
+@Component({
+    selector: "ns-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+    @ViewChild('password') passwordField: ElementRef;
+    isAuthenticating = false;
+
+    public hideIcon = String.fromCharCode(0xf070);
+    public showIcon = String.fromCharCode(0xf06e);
+    public showHideIcon: any;
+    private showPassword = false;
+
+    constructor(
+      private page: Page,
+      private routerExtensions: RouterExtensions,
+    ) {
+    }
+
+    ngOnInit() {
+      //this.page.actionBarHidden = true;
+      //this.page.cssClasses.add("login-page-background");
+      //this.page.backgroundSpanUnderStatusBar = true;
+      this.showHideIcon = this.hideIcon;
+    }
+
+    showHidePassword() {
+      this.showPassword = !this.showPassword;
+      this.showHideIcon = this.showPassword ? this.showIcon : this.hideIcon;
+      let passField: TextField = this.passwordField.nativeElement;
+      passField.secure = !passField.secure;
+    }
+  }
---
+++ AppEmpresa/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { NativeScriptRouterModule } from "@nativescript/angular";
+
+import { ItemsComponent } from "./item/items.component";
+import { ItemDetailComponent } from "./item/item-detail.component";
+import { LoginComponent } from "./Login/login.component";
+
+const routes: Routes = [
+    { path: "", component: LoginComponent },
+    { path: "items", component: ItemsComponent },
+    { path: "item/:id", component: ItemDetailComponent }
+];
+
+@NgModule({
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule]
+})
+export class AppRoutingModule { }
