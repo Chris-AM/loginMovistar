74c95e02b76054649cb62e2fcc985b96b6ce8ec8
Chris-AM <c.aranguizm@outlook.com>
2020-11-11 17:44:01 -0300

    endpoints refactoring

--- AppEmpresa/src/app/listarEmpresas/empresa.service.ts
+++ AppEmpresa/src/app/listarEmpresas/empresa.service.ts
@@ -1,18 +1,20 @@
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
-
+import { Endpoints } from '../shared/endpoints/endpoints';
 @Injectable({
     providedIn: "root"
 })
 export class EmpresaService {
+
+    private url = new Endpoints();
 
     constructor(private http: HttpClient) { }
 
     getEmpresas(rut, access_token): Observable<any> {
 
         var token=access_token;
-        let uri = `https://apix.movistar.cl/sitecorporate/V1/contact/${rut}/customers`
+        let uri = `${this.url.url_base}`+`${this.url.access}`+`${rut}`+`${this.url.customers}`
         return this.http.get(uri,
         {
             headers: new HttpHeaders().set('Authorization', 'Bearer '+token)  
--- AppEmpresa/src/app/shared/endpoints/endpoints.ts
+++ AppEmpresa/src/app/shared/endpoints/endpoints.ts
@@ -1,4 +1,10 @@
-export class endpoints {
+export class Endpoints {
     //rutas
-    url_base = 'https://apix.movistar.cl/';
+    url_base = 'https://apix.movistar.cl';
+    login = '/oauth2/login-app/loginCajetin?apikey=d01d4fb6-de04-4662-9aa9-7a3fa06cb5c6';
+    authorization = '/oauth2/userAuthorize';
+    token = '/oauth2/token';
+    stepThreeKey = 'https://myloginOauth.net/auth-code';
+    access = '/sitecorporate/V1/contact/';
+    customers = '/customers';
 }
