526dbfaaee6e20efe8a6c04337775a9817d4404f
fescobab <fescobab@everis.com>
2020-11-16 19:29:22 -0300

    estilo de la flecha

--- AppEmpresa/src/app/shared/refactoringTexts.ts
+++ AppEmpresa/src/app/shared/refactoringTexts.ts
@@ -1,14 +1,14 @@
 
 export class refactoringText {
     //textos de la app movistar empresa
-    
-    text1 = "Ingresa tu rut de cliente";
+
+    text1 = "Ingresa tu RUT de cliente";
     textError = "Usuario y/o contraseña incorrectos. Intenta nuevamente."
-    
+
     text2 = "Tu contraseña de sucursal virtual";
     welcome = '¡Nos alegra verte aquí!';
     info = 'Ingresa a conocer el resumen y consumo de tus cuentas';
-    rememberPass = 'Recordar mi Rut';
+    rememberPass = 'Recordar mi RUT';
     passForgotten = 'Si olvidaste tu contraseña, haz clic aquí';
-    
+
 }
